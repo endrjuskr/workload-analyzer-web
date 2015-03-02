72a392d380a82ed34d9314a21ad3a15fc538e077
Andrzej Skrodzki <andrzej.skrodzki@gmail.com>
2015-03-02 12:40:07 +0100

    Workloads.

--- workload/admin.py
+++ workload/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
-from workload.models import Machine, Task, Execution, ExecutionParam, Workload
+from workload.models import *
 
 admin.site.register(Machine)
 admin.site.register(Task)
 admin.site.register(Execution)
 admin.site.register(ExecutionParam)
+admin.site.register(ExecutionResult)
+admin.site.register(WorkloadResult)
+admin.site.register(Comment)
 admin.site.register(Workload)
